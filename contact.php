46b1cd32f054cd3f973043bf12c0d1ff6272435f
BAYU SENO AJI <you@example.com>
2021-04-22 09:04:04 +0700

    first commit

---
+++ contact.php
@@ -0,0 +1,50 @@
+$notify = '';
+$notifyClass = '';
+if(isset($_POST['submit'])){
+// Get the submitted form data
+$email = $_POST['email'];
+$name = $_POST['name'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+// Cek apakah ada data yang belum diisi
+if(!empty($email) && !empty($name) && !empty($subject) && !empty($message)){
+
+if(filter_var($email, FILTER_VALIDATE_EMAIL) === false){
+$notify = 'Email Anda salah. Silakan ketikan alamat email yang benar.';
+$notifyClass = 'errordiv';
+}else{
+// Pengaturan penerima email dan subjek email
+$toEmail = 'turanzafox@gmail.com'; // Ganti dengan alamat email yang Anda inginkan
+$emailSubject = 'Pesan website dari '.$name;
+$htmlContent = '<h2> via Form Kontak Website</h2>
+<h4>Name</h4>
+<p>'.$name.'</p>
+<h4>Email</h4>
+<p>'.$email.'</p>
+<h4>Subject</h4>
+<p>'.$subject.'</p>
+<h4>Message</h4>
+<p>'.$message.'</p>';
+
+// Mengatur Content-Type header untuk mengirim email dalam bentuk HTML
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+// Header tambahan
+$headers .= 'From: '.$name.'<'.$email.'>'. "\r\n";
+
+    // Send email
+    if(mail($toEmail,$emailSubject,$htmlContent,$headers)){
+    $notify = 'Pesan Anda sudah terkirim dengan sukses !';
+    $notifyClass = 'succdiv';
+    }else{
+    $notify = 'Maaf pesan Anda gagal terkirim, silahkan ulangi lagi.';
+    $notifyClass = 'errordiv';
+    }
+    }
+    }else{
+    $notify = 'Harap mengisi semua field data';
+    $notifyClass = 'errordiv';
+    }
+    }
